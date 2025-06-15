3f03b2a2311702a28300a91791b0800b9e63899f
Richa Lahoti <rlahoti39141@ucumberlands.edu>
2025-06-14 21:26:40 -0700

    add in deterministic quicksort and capture results

--- assignment_3.py
+++ assignment_3.py
@@ -1,30 +1,38 @@
 import random
 import time
 
-def random_quicksort(i):
+def quicksort_wrapper(i, strategy="random"):
     if not isinstance(i, list):
         return None
     if len(i) <= 1:
         return i.copy()
     
     ic = i.copy()
-    quicksort(ic, 0, len(ic) - 1)
+    quicksort(ic, 0, len(ic) - 1, strategy)
     return ic
 
-def quicksort(i, l, h):
+def quicksort(i, l, h, strategy):
     while l < h:
-        pivot_idx = random_partition(i, l, h)
+        if strategy == "random":
+            pivot_idx = random_partition(i, l, h)
+        else:
+            pivot_idx = first_element_partition(i, l, h)
+
         
         if pivot_idx - l < h - pivot_idx:
-            quicksort(i, l, pivot_idx - 1)
+            quicksort(i, l, pivot_idx - 1, strategy)
             l = pivot_idx + 1
         else:
-            quicksort(i, pivot_idx + 1, h)
+            quicksort(i, pivot_idx + 1, h, strategy)
             h = pivot_idx - 1
 
 def random_partition(i, l, h):
     pivot_idx = random.randint(l, h)
     i[pivot_idx], i[h] = i[h], i[pivot_idx]
+    return partition(i, l, h)
+
+def first_element_partition(i, l, h):
+    i[l], i[h] = i[h], i[l]
     return partition(i, l, h)
 
 def partition(i, l, h):
@@ -68,7 +76,7 @@ def part_1_validation():
     repeated_elements = generate_repeated_data(10)
 
     for array_type in (sorted_array, empty_array, repeated_elements):
-        out = random_quicksort(array_type)
+        out = quicksort_wrapper(array_type, strategy="random")
         array_type.sort()
         if array_type != out:
             print("part 1 validation failed")
@@ -83,30 +91,58 @@ def part_1_comparison():
 
     # randomized quicksort on random data
     start_time = time.time()
-    random_quicksort(random_data)
+    quicksort_wrapper(random_data, strategy="random")
     end_time = time.time()
     print(">>>Randomized Quicksort on Random Data")
     print("took " + str(end_time - start_time) + " seconds\n")
 
     # randomized quicksort on sorted data
     start_time = time.time()
-    random_quicksort(sorted_data)
+    quicksort_wrapper(sorted_data, strategy="random")
     end_time = time.time()
     print(">>>Randomized Quicksort on Sorted Data")
     print("took " + str(end_time - start_time) + " seconds\n")
 
     # randomized quicksort on reversed sorted data
     start_time = time.time()
-    random_quicksort(reversed_sorted_data)
+    quicksort_wrapper(reversed_sorted_data, strategy="random")
     end_time = time.time()
     print(">>>Randomized Quicksort on Reversed Sorted Data")
     print("took " + str(end_time - start_time) + " seconds\n")
 
     # randomized quicksort on repeated random data
     start_time = time.time()
-    random_quicksort(repeated_data)
+    quicksort_wrapper(repeated_data, strategy="random")
     end_time = time.time()
     print(">>>Randomized Quicksort on Repeated Random Data")
+    print("took " + str(end_time - start_time) + " seconds\n")
+
+    # deterministic quicksort on random data
+    start_time = time.time()
+    quicksort_wrapper(random_data, strategy="deterministic")
+    end_time = time.time()
+    print(">>>Deterministic Quicksort on Random Data")
+    print("took " + str(end_time - start_time) + " seconds\n")
+
+    # deterministic quicksort on sorted data
+    start_time = time.time()
+    quicksort_wrapper(sorted_data, strategy="deterministic")
+    end_time = time.time()
+    print(">>>Deterministic Quicksort on Sorted Data")
+    print("took " + str(end_time - start_time) + " seconds\n")
+
+    # deterministic quicksort on reversed sorted data
+    start_time = time.time()
+    quicksort_wrapper(reversed_sorted_data, strategy="deterministic")
+    end_time = time.time()
+    print(">>>Deterministic Quicksort on Reversed Sorted Data")
+    print("took " + str(end_time - start_time) + " seconds\n")
+
+    # deterministic quicksort on repeated random data
+    start_time = time.time()
+    quicksort_wrapper(repeated_data, strategy="deterministic")
+    end_time = time.time()
+    print(">>>Deterministic Quicksort on Repeated Random Data")
     print("took " + str(end_time - start_time) + " seconds\n")
 
 if __name__ == "__main__":
